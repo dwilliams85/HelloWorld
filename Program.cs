aeaac41f65e5a996ed1b10c0ff97eae9913c18d9
Darrell Williams <dwilliams10@usf.edu>
2019-09-07 12:57:22 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause Hello World message
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Are you sure you want to continue...");
+            Console.ReadKey();
+        }
+    }
+}
